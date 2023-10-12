6f3198854dd39b9dabe231c9d6953d4754498e0a
icaruuuuu <ericamaelazartecape@gmail.com>
2023-10-12 20:21:21 +0800

    Initial commit

---
+++ grading_system_emc/src/app/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+import { TableComponent } from './table/table.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    TableComponent
+    ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    CardComponent,
+    TableComponent
+  ],
+
+})
+export class ComponentsModule { }
